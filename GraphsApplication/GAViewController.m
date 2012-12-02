fb7c84553aa2d61f1bb14a4df8366dc2e04ab364
Rogelio Gudino <cananito@gmail.com>
2012-12-02 13:28:39 -0800

    Added map interaction.

--- GraphsApplication/GAViewController.m
+++ GraphsApplication/GAViewController.m
@@ -9,6 +9,8 @@
 #import "GAViewController.h"
 
 @interface GAViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *mapView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *modeSegmentedControl;
 
 @end
@@ -17,13 +19,61 @@
 
 - (void)viewDidLoad
 {
+    UITapGestureRecognizer *tapGestureRecognizer;
     
     [super viewDidLoad];
+    
+    tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(mapWasTapped:)];
+    [self.mapView addGestureRecognizer:tapGestureRecognizer];
 }
 
+
+#pragma mark - Action Methods
 - (IBAction)modeValueChanged:(UISegmentedControl *)sender
 {
-    
+    //Clear current state
+}
+
+- (void)mapWasTapped:(UITapGestureRecognizer *)tapGestureRecognizer
+{
+    switch (self.modeSegmentedControl.selectedSegmentIndex) {
+        case 0:
+            [self addVertexWasTapped:tapGestureRecognizer];
+            break;
+        case 1:
+            [self addEdgeWasTapped:tapGestureRecognizer];
+            break;
+        case 2:
+            [self dijkstraWasTapped:tapGestureRecognizer];
+            break;
+        case 3:
+            [self deleteWasTapped:tapGestureRecognizer];
+            break;
+        default:
+            break;
+    }
+}
+
+
+#pragma mark - Private Methods
+- (void)addVertexWasTapped:(UITapGestureRecognizer *)tapGestureRecognizer
+{
+    NSLog(@"VERTEX");
+}
+
+- (void)addEdgeWasTapped:(UITapGestureRecognizer *)tapGestureRecongnizer
+{
+    NSLog(@"EDGE");
+}
+
+- (void)dijkstraWasTapped:(UITapGestureRecognizer *)tapGestureRecongnizer
+{
+    NSLog(@"DIJSKTRA");
+}
+
+- (void)deleteWasTapped:(UITapGestureRecognizer *)tapGestureRecongnizer
+{
+    NSLog(@"DELETE");
 }
 
 @end
